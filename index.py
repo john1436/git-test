4fc88ad6105a4b5880c744321514659ddaa10dab
John <john14361gilbert@gmail.com>
2025-10-19 12:17:48 +0530

    aded the a feature

--- index.py
+++ index.py
@@ -1,2 +1,9 @@
 def function_a():
     print("A is running")
+    print("A is running")
+    print("A is running")
+    print("A is running")
+    print("A is running")
+    print("A is running")
+    print("A is running")
+
